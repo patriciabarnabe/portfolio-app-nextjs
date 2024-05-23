1fa5536265d667a8928d3ae09148448fb4a947aa
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-23 16:23:35 -0300

    Creating hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,58 @@
+//Hero file typically stands for the primary section that is seen when the user first enters the page
+
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaArrowTurnDown } from "react-icons/fa6";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      {/* The div below acts as a wrapper for the first components coming from Aceternity */}
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      {/* The div below acts like a UI grid */}
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Patrícia, a Frontend Developer based in Brazil.
+          </p>
+
+          <Link href="#about">
+            <MagicButton
+              title="See my work"
+              icon={<FaArrowTurnDown />}
+              position="right"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ utils/cn.ts
@@ -0,0 +1,8 @@
+//Helper function to make everything work together with Tailwind
+
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
