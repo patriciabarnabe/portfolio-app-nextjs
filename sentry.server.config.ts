b234134ef18994e8ae833ae4c5f5d07b5df98cf4
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-27 16:52:26 -0300

    Sentry setup and adding user feedback  feature

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4d46e934a84f3a9bd7a1f9acd0001929@o4507329916829696.ingest.us.sentry.io/4507329924562944",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
