a6a7c37a5065f2de382f6ad2e000a799cefab049
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-28 14:37:54 -0300

    Creating footer section

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
