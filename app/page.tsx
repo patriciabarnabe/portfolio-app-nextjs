4daf4e363d68af052f7c03883f9e761b23d07197
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-28 11:19:01 -0300

    Creating my work experience section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
---
+++ components/Experience.tsx
@@ -0,0 +1,41 @@
+import { workExperience } from "@/data";
+import { Button } from "./ui/MovingBorder";
+
+const Experience = () => {
+  return (
+    <div className="py-20">
+      <h1 className="heading">
+        My
+        <span className="text-purple"> work experience</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map(({ id, title, desc, className, thumbnail }) => (
+          <Button
+            key={id}
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            className="flex-1 text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={thumbnail}
+                alt={title}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {desc}
+                </p>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
